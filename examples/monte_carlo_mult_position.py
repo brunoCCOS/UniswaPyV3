64f79229472fbf1e059b4f575d800256022b02ff
Bruno Trotti <--list>
2024-11-09 08:51:22 -0300

    new swap mechanism and pool checking liquidity feature

--- examples/monte_carlo_mult_position.py
+++ examples/monte_carlo_mult_position.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from uniswapyv3.utils import generate_poisson_arrivals
 from uniswapyv3.pool import LiquidityPool
-# import pool
+import itertools
 
 
-NUM_SIMULATIONS = 1000
+NUM_SIMULATIONS = 2000
 
 # Initialize parameters
 INITIAL_PRICE = 3000
@@ -19,87 +19,90 @@ PRICE_RANGES = [
     (INITIAL_PRICE/3, INITIAL_PRICE*3),
 ]
 PORTFOLIO_VALUE = 100
-TIME = 24*7
+TIME = [24,24*7]
 LAMBDA_PARAM = 222
 MU = 0.00005
-SIGMA = 0.07
+SIGMA = [0.07,0.007]
+params = list(itertools.product(TIME, SIGMA))
+print(params)
+for time,sigma in params:
+    print(time,sigma)
+    keys = [f'{pr[0]}-{pr[1]}' for pr in PRICE_RANGES]
+
+    # Initialize lists to store results
+    impermanent_losses = {key : [] for key in keys}
+    fees_collected = {key : [] for key in keys}
+    total_return = {key : [] for key in keys}
+
+    all_prices = []
 
 
+    for _ in range(NUM_SIMULATIONS):
+        arrival_times = generate_poisson_arrivals(LAMBDA_PARAM, time)
+        price = INITIAL_PRICE
 
-keys = [f'{pr[0]}-{pr[1]}' for pr in PRICE_RANGES]
+        # Create a liquidity pool with specified parameters
+        pool = LiquidityPool(
+            tick_space = 2,
+            fee = 0.003,
+            tick_size = 1.0001,
+            initial_price = INITIAL_PRICE,
+        )
 
-# Initialize lists to store results
-impermanent_losses = {key : [] for key in keys}
-fees_collected = {key : [] for key in keys}
-total_return = {key : [] for key in keys}
+        # Add the provider to the liquidity pool
+        positions = [
+                pool.open_position(*price_range,V = PORTFOLIO_VALUE) for price_range in PRICE_RANGES
+        ]
 
-all_prices = []
+        for t in range(1, len(arrival_times)):
+            dt = arrival_times[t] - arrival_times[t-1]
+            dW = np.random.normal(0, np.sqrt(dt))
+            price =  price * (1 + MU * dt + sigma * dW)
+            pool.update_price(new_price=price)
 
-
-for _ in range(NUM_SIMULATIONS):
-    arrival_times = generate_poisson_arrivals(LAMBDA_PARAM, TIME)
-    price = INITIAL_PRICE
-
-    # Create a liquidity pool with specified parameters
-    pool = LiquidityPool(
-        tick_space = 2,
-        fee = 0.003,
-        tick_size = 1.0001,
-        initial_price = INITIAL_PRICE,
-    )
-
-    # Add the provider to the liquidity pool
-    positions = [
-            pool.open_position(*price_range,V = PORTFOLIO_VALUE) for price_range in PRICE_RANGES
-    ]
-
-    for t in range(1, len(arrival_times)):
-        dt = arrival_times[t] - arrival_times[t-1]
-        dW = np.random.normal(0, np.sqrt(dt))
-        price =  price * (1 + MU * dt + SIGMA * dW)
-        pool.update_price(new_price=price)
-
-    for idx, key in enumerate(keys):
-        impermanent_losses[key].append(positions[idx].calculate_il())
-        total_return[key].append(positions[idx].calculate_total_return())
-        fees_collected[key].append(positions[idx].fees_withdraw)
+        for idx, key in enumerate(keys):
+            impermanent_losses[key].append(positions[idx].calculate_il())
+            total_return[key].append(positions[idx].calculate_total_return())
+            fees_collected[key].append(positions[idx].fees_withdraw)
 
 
-# Set the ggplot style
-plt.style.use('fast')
-sns.set_palette("muted")
+    # Set the ggplot style
+    plt.style.use('fast')
+    sns.set_palette("muted")
 
-# Create the first set of histograms
-fig, axes = plt.subplots(1, 2, figsize=(14, 10))
-fig.suptitle('Simulation Results', fontsize=18, weight='bold')
+    # Create the first set of histograms
+    fig, axes = plt.subplots(1, 2, figsize=(18, 10))
+    fig.suptitle('Simulation Results', fontsize=18, weight='bold')
 
-# Histogram of Impermanent Losses
-sns.histplot(impermanent_losses, bins=100, kde=True, color='darkblue', ax=axes[0], alpha=0.7)
-axes[0].set_title('Impermanent Loss Distribution', fontsize=14)
-axes[0].set_xlabel('Prices', fontsize=12)
-axes[0].set_ylabel('Frequency', fontsize=12)
+    # Histogram of Impermanent Losses
+    sns.histplot(impermanent_losses, bins=100, kde=True, color='darkblue', ax=axes[0], alpha=0.7)
+    axes[0].set_title('Impermanent Loss Distribution', fontsize=16)
+    axes[0].set_xlabel('Loss', fontsize=16)
+    axes[0].set_ylabel('Frequency', fontsize=12)
+    axes[0].tick_params(axis = 'both' , labelsize= 15)
+    # Histogram of Fees Collected
+    sns.histplot(fees_collected, bins=100, kde=False, color='darkgreen', ax=axes[1], alpha=0.7)
+    axes[1].set_title('Fees Distribution', fontsize=16)
+    axes[1].set_xlabel('Fees Collected', fontsize=16)
+    axes[1].set_ylabel('Frequency', fontsize=12)
+    axes[1].tick_params(axis = 'both' , labelsize= 15)
 
-# Histogram of Fees Collected
-sns.histplot(fees_collected, bins=100, kde=False, color='darkgreen', ax=axes[1], alpha=0.7)
-axes[1].set_title('Fees Distribution', fontsize=14)
-axes[1].set_xlabel('Fees Collected', fontsize=12)
-axes[1].set_ylabel('Frequency', fontsize=12)
+    # Adjust layout for better spacing
+    plt.tight_layout(rect=(0, 0, 1, 0.96))
 
-# Adjust layout for better spacing
-plt.tight_layout(rect=(0, 0, 1, 0.96))
+    # Save the first figure with high resolution
+    plt.savefig(f'figures/il_taxes_T={time}_sigma={sigma}_mu={MU}.png', dpi=300)
+    # plt.show()
 
-# Save the first figure with high resolution
-plt.savefig(f'figures/il_taxes_T={TIME}_sigma={SIGMA}_mu={MU}.png', dpi=300)
-plt.show()
+    # Plot Total Return Distribution
+    fig, axes = plt.subplots(1, 1, figsize=(14, 10))
+    sns.histplot(total_return, bins=100, kde=False, color='purple', ax=axes, alpha=0.7)
+    axes.set_title('Total Return Distribution', fontsize=16)
+    axes.set_xlabel('Total Return', fontsize=16)
+    axes.set_ylabel('Frequency', fontsize=12)
+    axes.tick_params(axis = 'both' ,which='major', labelsize= 15)
 
-# Plot Total Return Distribution
-fig, axes = plt.subplots(1, 1, figsize=(14, 10))
-sns.histplot(total_return, bins=100, kde=False, color='purple', ax=axes, alpha=0.7)
-axes.set_title('Total Return Distribution', fontsize=14)
-axes.set_xlabel('Total Return', fontsize=12)
-axes.set_ylabel('Frequency', fontsize=12)
-
-# Adjust layout and save the second figure
-plt.tight_layout(rect=(0, 0, 1, 0.96))
-plt.savefig(f'figures/results_T={TIME}_sigma={SIGMA}_mu={MU}.png', dpi=300)
-plt.show()
+    # Adjust layout and save the second figure
+    plt.tight_layout(rect=(0, 0, 1, 0.96))
+    plt.savefig(f'figures/results_T={time}_sigma={sigma}_mu={MU}.png', dpi=300)
+    # plt.show()
--- uniswapyv3/pool.py
+++ uniswapyv3/pool.py
@@ -1,4 +1,3 @@
-from matplotlib.pyplot import tick_params
 import numpy as np
 from .position import LiquidityPosition
 from .utils import smallest_divisor
@@ -27,6 +26,21 @@ class LiquidityPool:
         self.sqrt_price: float = np.sqrt(initial_price)  # Current price level in the pool
         self.current_tick: int = self._price_to_tick(self.sqrt_price**2)  # Current tick in the pool
         self.liquidity:float = 0
+
+    @staticmethod
+    def _calc_delta_x(liquidity: float, current_price: float, future_price: float) -> float:
+        return liquidity * (1 / current_price  - 1 / future_price)
+
+    @staticmethod
+    def _calc_delta_y(liquidity: float, current_price: float, future_price: float) -> float:
+        return liquidity * (current_price - future_price)
+
+    @staticmethod
+    def _calc_future_price(liquidity: float, current_price: float, tokens: float) -> float:
+        if tokens > 0:
+            return current_price - liquidity/tokens
+        else:
+            return current_price - tokens/liquidity
 
     def open_position(self, min_price: float, max_price: float, V: float) -> LiquidityPosition:
         """
@@ -67,22 +81,92 @@ class LiquidityPool:
         self.ticks_liquidity[lower_idx:upper_idx + 1] += position.liquidity
         self.liquidity += position.liquidity
 
+    def swap(self, token):
+        """
+        Swap a token ammount
+
+        :param token: Number of tokens to exchange, if positive then exchange token X for Y, if negative, token Y for X
+        """
+        if token == 0:
+            print("Total ammount of tokens must be non-zero")
+            return
+
+        current_tick = self.current_tick
+        current_price = self.sqrt_price
+
+        # Extracts the fees and the amount of token avaible for swap
+        token = token*(1-self.fee)
+
+        # Define the direction of thw swap
+        if token > 0:
+            direction = 1
+        else:
+            direction = -1
+
+        # Dict to store ammount of fees paid in each tick
+        fees_dict = {}
+        # Define future price as the actual price initially 
+        future_price = current_price
+
+        # Deplete all ticks until the swap is fullfilled
+        while token > 0:
+            current_liquidity = self.ticks_liquidity[current_tick]
+            if current_liquidity == 0:
+                print("Not enough resources to fullfill the swap")
+                return
+
+            
+            # If buying token X, price goes UP
+            if direction == 1:
+                # Get the max betwen the price inside the tick and the target price
+                # of the swap, if the target price is below, that means that the Swap
+                # does not cross any tick, otherwise we need to make the swap within
+                # the current tick and then waste the rest in next tick
+                future_price = max(
+                    self._tick_to_sqrt_price(current_tick + self.tick_space),
+                    self._calc_future_price(current_liquidity,current_price,token)
+                    )
+                delta = self._calc_delta_y(current_liquidity,current_price,future_price)
+                fees_dict[current_tick] = np.array([0,delta / (delta*(1-self.fee))])
+            # Else if buying token Y, price goes down
+            else:
+                # Get the min betwen the price inside the tick and the target price
+                # of the swap, if the target price is above, that means that the Swap
+                # does not cross any tick_space
+                future_price = min(
+                    self._tick_to_sqrt_price(current_tick),
+                    self._calc_future_price(current_liquidity,current_price,token)
+                    )
+                delta = self._calc_delta_x(current_liquidity,current_price,future_price)
+                fees_dict[current_tick] = np.array([delta / (delta*(1-self.fee)),0])
+
+            # Remove the amount of tokens already swaped from the remaining total
+            token -= delta
+
+        # IF the entire trade succeed then modify the values of the pool
+        
+        #Distribute the fees in each tick
+        for tick,fees_paid in fees_dict.items():
+            self._distribute_fees(self.current_tick, fees_paid, self.ticks_liquidity[tick])
+        self.sqrt_price = future_price
+
     def update_price(self, new_price: float):
         """
         Updates the price in the pool and triggers fee collection based on price movement.
 
         :param new_price: The new price to update in the pool.
         """
-        current_sqrt_price = self.sqrt_price
-        current_tick = self.current_tick
-
-
         new_tick = self._price_to_tick(new_price)
 
-        if new_tick > self.upper_tick:
-            self._initialize_ticks(current_sqrt_price**2, new_price)
-        elif new_tick < self.lower_tick:
-            self._initialize_ticks(new_price, current_sqrt_price**2)
+        # Check if any tick until the final price has any liqudity 
+        # and is possible to complete the trade
+        if any(self.ticks_liquidity[self._get_index_tick(self.current_tick):self._get_index_tick(new_tick)]) == 0:
+            print("Not enough resources to fullfill the trade")
+            print("One of the ticks betwen the current price and the desired spot are wihtout liquidity")
+            return
+
+        current_sqrt_price = self.sqrt_price
+        current_tick = self.current_tick
 
         direction = 1 if new_price > current_sqrt_price**2 else -1
 
@@ -91,19 +175,19 @@ class LiquidityPool:
 
             tick_idx: int = self._get_tick_index(tick)
             tick_liquidity: float = self.ticks_liquidity[tick_idx]
-
+            
             if direction == -1:
                 # Amount of virtual token X received for the price going from the current price to the left tick of the interval
                 future_price = self._tick_to_sqrt_price(current_tick)
-                delta: float = tick_liquidity * (1 / future_price - 1 / current_sqrt_price)
+                delta: float = -self._calc_delta_x(tick_liquidity,future_price,current_sqrt_price)
                 self.current_tick = current_tick = current_tick - self.tick_space
-                fees_paid = np.array([delta * self.fee,0])
+                fees_paid = np.array([delta / (delta*(1-self.fee)),0])
             else:
                 # Amount of virtual token Y received for the price going from the current price to the right tick of the interval
                 self.current_tick = current_tick = current_tick + self.tick_space
                 future_price = self._tick_to_sqrt_price(current_tick)
-                delta: float = tick_liquidity * (future_price - current_sqrt_price)
-                fees_paid = np.array([0,delta * self.fee])
+                delta: float = -self._calc_delta_y(tick_liquidity,future_price,current_sqrt_price)
+                fees_paid = np.array([0,delta / (delta*(1-self.fee))])
 
 
             self._distribute_fees(tick, fees_paid, tick_liquidity)
@@ -114,14 +198,17 @@ class LiquidityPool:
         future_price = np.sqrt(new_price)
 
         if direction == -1:
-            delta: float = tick_liquidity * (1 / future_price - 1 / current_sqrt_price)
+            delta: float = self._calc_delta_y(tick_liquidity,future_price,current_sqrt_price)
             fees_paid = np.array([delta / (1 - self.fee),0])
         else:
-            delta: float = tick_liquidity * (future_price - current_sqrt_price)
+            delta: float = self._calc_delta_x(tick_liquidity,future_price,current_sqrt_price)
             fees_paid = np.array([0,delta / (1 - self.fee)])
 
         self._distribute_fees(self.current_tick, fees_paid, tick_liquidity)
         self.sqrt_price = future_price
+
+
+
 
     def _distribute_fees(self, tick: int, fees_paid: np.ndarray, tick_liquidity: float) -> None:
         """
@@ -202,4 +289,4 @@ class LiquidityPool:
         :return: The liquidity at the specified tick.
         """
         tick_idx = self._get_tick_index(tick)
-        return self.ticks_liquidity[tick_idx]
+        return self.ticks_liquidity[tick_idx]        
